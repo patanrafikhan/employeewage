698d7c91109590422e439b65ea66bdbfc96a45f5
patanrafikhan <iamkhan342@gmail.com>
2023-01-28 15:31:52 +0530

    Added ; initial message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePresentand_Absent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Employee wage App");
+        }
+    }
+}
